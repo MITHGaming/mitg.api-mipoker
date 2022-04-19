78bb2871622f2e0e3674e444e113984327cb1638
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-04-18 21:32:32 -0400

    feat: add prisma and default model

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,17 @@
-import { io, server } from '@/main/config/socketio'
-import app from '@/main/config/app'
 import env from '@/environment'
+import { PrismaClient } from '@prisma/client'
 
-app.set('socketio', io)
-app.set('server', server)
+const Prisma = new PrismaClient()
 
-server.listen(env.APP.PORT, () => {
-  console.log(`Server running at http://localhost:${env.APP.PORT}`)
-  console.log(`Server is running with ENV_PRODUCTION: ${env.APP.PRODUCTION}`)
+Prisma.$connect().then(async () => {
+  const app = (await import('../main/config/app')).default
+  const { io, server } = await import('../main/config/socketio')
+
+  app.set('socketio', io)
+  app.set('server', server)
+
+  server.listen(env.APP.PORT, () => {
+    console.log(`Server running at http://localhost:${env.APP.PORT}`)
+    console.log(`Server is running with ENV_PRODUCTION: ${env.APP.PRODUCTION}`)
+  })
 })

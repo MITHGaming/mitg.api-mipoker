bb116e1ad7cd7811ff6014214b14f4800649038c
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-04-21 20:57:44 -0400

    refactor: make receive json

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -9,10 +9,10 @@ import {
 } from '@/main/middleware'
 
 export default (app: Express): void => {
+  app.use(bodyParser)
   app.use(corsInternal)
   app.use(cookieParser)
   app.use(protections)
-  app.use(bodyParser)
   app.use(handleJson)
   app.use(contentType)
 }
